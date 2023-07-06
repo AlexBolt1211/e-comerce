7b543c3ce9094947002eee89a323fffdc75582c0
Alex Bolt <bolt.izh@>
2023-07-07 01:09:09 +0300

    message

--- src/main/java/com/example/application/EComerceApplication.java
+++ src/main/java/com/example/application/EComerceApplication.java
@@ -1,5 +1,13 @@
 package com.example.application;
 
+/**
+ * The project is an E-commerce platform and represents an online store for selling goods.
+ *
+ * The composition of the project is based on the MVC (Model-View-Controller) architectural pattern, which allows for
+ * the separation of application logic, data representation, and user interface. The project code is written in the Java
+ * programming language using the Spring framework for web application development.
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
